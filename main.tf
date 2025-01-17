d65451dc56a917c53c52d4e37fcebee5fdc8ea09
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-01-17 21:24:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,41 +26,91 @@ resource "google_compute_instance" "vm_instance" {
 
 }
 
-resource "google_os_config_os_policy_assignment" "apache_install" {
-  name        = "apache-install"
-  project     = var.project_id
-  description = "Install Apache on existing VM"
-  location    = "us-central1-a"
+# resource "google_os_config_os_policy_assignment" "apache_install" {
+#   name        = "apache-install"
+#   project     = var.project_id
+#   description = "Install Apache on existing VM"
+#   location    = "us-central1-a"
+
+#   instance_filter {
+#     all = false
+#     inclusion_labels {
+#       labels = {
+#         env = "production"
+#       }
+#     }
+#     inventories {
+#       os_short_name = "debian"
+#     }
+#   }
+#   os_policies {
+#     id   = "install-apache"
+#     mode = "ENFORCEMENT"
+#     resource_groups {
+#       resources {
+#         id = "install-apache-pkg"
+#         pkg {
+#           desired_state = "INSTALLED"
+#           name          = "apache2"
+#         }
+#       }
+#     }
+#   }
+
+#   rollout {
+#     disruption_budget {
+#       fixed = 1
+#     }
+#     min_wait_duration = "60s"
+#   }
+# }
+
+resource "google_os_config_os_policy_assignment" "install-google-cloud-ops-agent" {
+  description = "Install the ops agent on hosts"
+  location    = var.zone
+  name        = "install-google-cloud-ops-agent"
+  project     = var.project
 
   instance_filter {
     all = false
-    inclusion_labels {
-      labels = {
-        env = "production"
-      }
-    }
     inventories {
-      os_short_name = "debian"
+      os_short_name = "ubuntu"
+      os_version    = "22.04"
     }
   }
+
   os_policies {
-    id   = "install-apache"
-    mode = "ENFORCEMENT"
+    allow_no_resource_group_match = false
+    description                   = "Copy file from GCS bucket to local path"
+    id                            = "install-and-configure-ops-agent"
+    mode                          = "ENFORCEMENT"
+
     resource_groups {
       resources {
-        id = "install-apache-pkg"
-        pkg {
-          desired_state = "INSTALLED"
-          name          = "apache2"
+        id = "install-package"
+        exec {
+          enforce {
+            args        = []
+            interpreter = "SHELL"
+            script      = "apt-get install -y apache2 && exit 100"
+          }
+          validate {
+            args        = []
+            interpreter = "SHELL"
+            script      = "if systemctl is-active google-cloud-ops-agent-opentelemetry-collector; then exit 100; else exit 101; fi"
+          }
         }
       }
     }
   }
 
   rollout {
-    disruption_budget {
-      fixed = 1
-    }
     min_wait_duration = "60s"
+    disruption_budget {
+      fixed   = 0
+      percent = 100
+    }
   }
+
+  timeouts {}
 }
